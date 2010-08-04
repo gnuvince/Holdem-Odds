1e0b70d8f20c0f25a7bf2499dffecdfee092e53e
Vincent Foley <vfoley@gmail.com>
2010-08-04 18:46:49 -0400

    Used stdbool.h

--- src/cards.h
+++ src/cards.h
@@ -3,6 +3,7 @@
 #define _CARDS_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define RANKS_PER_DECK 13
 #define SUITS_PER_DECK 4
@@ -32,7 +33,7 @@ Card      NewCard(Rank, Suit);
 Card      NewCardFromChars(char, char);
 Card      NewCardFromString(char*);
 
-int       CardIsValid(const Card *);
+bool      CardIsValid(const Card *);
 Rank      CharToRank(char);
 Suit      CharToSuit(char);
 int       CardCompare(const Card*, const Card*);
